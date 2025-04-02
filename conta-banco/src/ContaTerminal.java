ea9c692400dfe23c6049a9c616ef612faa5faf09
danielzinhonog <danielcorreanogueira@outlook.com>
2025-04-02 16:46:33 -0300

    Finalizado o desadio de Simular uma Conta Bancária

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        //Mensagem de boas-vindas128
+        System.out.println("Bem-vindo ao Banco Java! Vamos criar sua conta!");
+
+        //TODO: Exibir as mensagens para o usuário e obter os valores digitados
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta: ");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+        //TODO: Exibir a mensagem da conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " + "sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+        
+        scanner.close();
     }
 }
